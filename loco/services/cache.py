1d316c0625f1842cf588b34961445c24e6b6eb92
Umesh Sharma <umesh@magicpin.in>
2017-12-02 12:12:35 +0530

    Convert user cache data to hash

--- loco/services/cache.py
+++ loco/services/cache.py
@@ -14,8 +14,11 @@ cache = redis.Redis(connection_pool=pool)
 KEY_LOCATION = "location"
 
 def set_user_location(user_id, location_data):
-	cache.set(user_id, {KEY_LOCATION:location_data})
+	cache.hmset(user_id, {KEY_LOCATION: location_data})
 
 def get_users_location(user_ids):
-	rows = cache.mget(user_ids)
-	return [row[KEY_LOCATION] if row else None for row in rows]
+	pipe = cache.pipeline()
+	for id in user_ids:
+		pipe.hget(id, KEY_LOCATION)
+	rows = pipe.execute()
+	return rows
