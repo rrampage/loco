867a27888105028ce4ea7cef0058d789e2d45bc5
Umesh Sharma <umesh@magicpin.in>
2017-12-02 12:42:44 +0530

    Update location cache logic

--- loco/services/cache.py
+++ loco/services/cache.py
@@ -1,4 +1,4 @@
-import redis
+import redis, pickle
 from django.conf import settings
 
 CACHE_LOCATION = 'loco.masterpeace.in'
@@ -11,14 +11,19 @@ db = CACHE_DB
 pool = redis.ConnectionPool(host=host, port=port, db=db, password="lokopass")
 cache = redis.Redis(connection_pool=pool)
 
-KEY_LOCATION = "location"
+KEY_LOCATION = "location_"
 
 def set_user_location(user_id, location_data):
-	cache.hmset(user_id, {KEY_LOCATION: location_data})
+	if not user_id or not location_data:
+		return
+		
+	key = KEY_LOCATION + str(user_id)
+	cache.set(key, pickle.dumps(location_data))
 
 def get_users_location(user_ids):
-	pipe = cache.pipeline()
-	for id in user_ids:
-		pipe.hget(id, KEY_LOCATION)
-	rows = pipe.execute()
-	return rows
+	if not user_ids:
+		return 
+
+	keys = [KEY_LOCATION+str(id) for id in user_ids]
+	rows = cache.mget(keys)
+	return [pickle.loads(row) if row else None for row in rows]
